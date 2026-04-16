0603ce4ccd3c1f75eb003cb03389be9525185451
Daniel Kleinman <danielkleinman70@gmail.com>
2026-04-16 11:57:44 -0400

    Initial commit

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -5,32 +5,58 @@ public class ListMaker {
         Scanner in = new  Scanner(System.in);
         String input = "";
         int num = 0;
+        int num2 = 0;
+        boolean needsToBeSaved = false;
         ArrayList<String> list = new ArrayList<String>();
         boolean done = false;
         do {
-            input = SafeInput.getRegExString(in,"Choose from the following options : [A]dd,[D]elete,[I]nsert,[P]rint,[Q]uit","^[AaDdIiPpQq]$");
+            input = SafeInput.getRegExString(in,"Choose from the following options : [A]dd,[D]elete,[I]nsert,[M]ove,[O]pen,[S]ave,[C]lear,[V]iew,[Q]uit","^[AaDdIiMmOoSsCcVvQq]$");
             switch (input.toUpperCase()) {
                 case "A":
                     input = SafeInput.getNonZeroLenString(in,"Enter string to insert");
                     list.add(input);
+                    needsToBeSaved = true;
                     break;
                 case "D":
                     num = getListPos(in,"Enter the item to remove", list);
                     list.remove(num);
+                    needsToBeSaved = true;
                     break;
                 case "I":
-                    num = getListPos(in,"Enter the item to insert", list);
-                    input = SafeInput.getNonZeroLenString(in,"Enter string to insert");
-                    list.remove(num);
-                    list.add(num, input);
-                    break;
-                case "P":
+                    if(!list.isEmpty()) {
+                        num = getListPos(in, "Enter the item to insert", list);
+                        input = SafeInput.getNonZeroLenString(in, "Enter string to insert");
+                        list.remove(num);
+                        list.add(num, input);
+                        needsToBeSaved = true;
+                    } break;
+                case "M":
+                    if (!list.isEmpty()) {
+                        num = getListPos(in, "Enter the item to move", list);
+                        num2 = getListPos(in, "Enter the new position", list);
+                        if (num < num2) {
+                            list.add(num2, list.get(num));
+                            list.remove(num);
+                        } else {
+                            list.add(num2, list.get(num));
+                            list.remove(num + 1);
+                        }
+                        needsToBeSaved = true;
+                    }
+                case "O":
+
+                case "S":
+
+                case "C":
+
+                case "V":
                     System.out.println("List:");
                     printList(list);
                     break;
                 case "Q":
                     done = SafeInput.getYNConfirm(in, "Are you sure you want to quit?");
                     break;
+
             }
         }while(!done);
     }
